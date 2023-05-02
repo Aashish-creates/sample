0ff540dce6ce9e3194073dea3f505294c37656cd
aashish subedi <aashishsubedi105@gmail.com>
2023-05-02 23:07:39 +0530

    second code

--- hekk.cpp
+++ hekk.cpp
@@ -1,10 +1,26 @@
-#include<iostream>
+
+// COUNT NO . OF CHAR IN STRING : ....
+
+//ASDFGVHJKLDMMK
+
+// ARR[26];
+
+//A->97
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-int a=10;
-cout<<a<<endl;
-return 0;
 
+    string s;
+    cin>>s;
+    
+    int arr[26]={0};
+    for(char c : s)
+     {
+         arr[c-'a']++;
+     }
+
+     for(int i=0;i<26;i++)
+       cout<<arr[i]<<" ";
 
 }
